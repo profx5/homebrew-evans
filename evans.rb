3f2e99ae253cffaa0fd95363ad6f35ad0b056852
lycoris0731 <aizu.s1230022@gmail.com>
2017-11-13 01:01:46 +0900

    Use CPU instead of Hardware

--- evans.rb
+++ evans.rb
@@ -2,15 +2,15 @@ class Evans < Formula
   desc "Evans: more expressive universal gRPC client"
   homepage "https://github.com/ktr0731/evans"
 
-  if Hardware.is_64_bit?
+  if CPU.is_64_bit?
     url "https://github.com/ktr0731/evans/releases/download/0.1.0/evans_darwin_amd64.tar.gz"
     sha256 "27077c846821b9a4cfdb4f3bef575537fd656fe2a026351e141ec0ae44e17ea3"
-  else 
+  else
     url "https://github.com/ktr0731/evans/releases/download/0.1.0/evans_darwin_386.tar.gz"
     sha256 "8677e8e9ee17ad1069305e985fda401fb511ef324e73d2f8ad0c4fbfe942face"
   end
 
   def install
-    bin.install evans
+    bin.install "evans"
   end
 end
