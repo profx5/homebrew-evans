4579f9a7a6850dc1a524b0bc0ab22147d40287db
ktr0731 <aizu.s1230022@gmail.com>
2018-02-02 12:30:40 +0000

    bump 0.1.2

--- evans.rb
+++ evans.rb
@@ -4,10 +4,10 @@ class Evans < Formula
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/ktr0731/evans/releases/download/0.1.2/evans_darwin_amd64.tar.gz"
-    sha256 ""
+    sha256 "6ac0119afb0c326c2d7e4abf2a133d9842f5ed03769c2e615f221227cc43f938"
   else
     url "https://github.com/ktr0731/evans/releases/download/0.1.2/evans_darwin_386.tar.gz"
-    sha256 ""
+    sha256 "c9be7fa51fd04866fec562af7cf2675be20049494b17c8217d25f97144399f3f"
   end
 
   def install
