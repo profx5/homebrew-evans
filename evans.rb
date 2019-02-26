ed4d652cb898d793bd4bd5d537cac455d37288b6
ktr0731 <aizu.s1230022@gmail.com>
2019-02-26 14:37:21 +0000

    bump 0.7.1

--- evans.rb
+++ evans.rb
@@ -1,9 +1,9 @@
 class Evans < Formula
   desc "Evans: more expressive universal gRPC client"
   homepage "https://github.com/ktr0731/evans"
-  url "https://github.com/ktr0731/evans/releases/download/0.7.0/evans_darwin_amd64.tar.gz"
-  sha256 "04f1b36d4d2cd9e37f6ac56fe3c2be7946a92fe20bdd90a3d648764756081659"
-  version "0.7.0"
+  url "https://github.com/ktr0731/evans/releases/download/0.7.1/evans_darwin_amd64.tar.gz"
+  sha256 "2bd5d9fe8e4b573624b9d4b70e8c70892a55183c319b8c545f92ded3828216bb"
+  version "0.7.1"
 
   def install
     bin.install "evans"
