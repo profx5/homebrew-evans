f3523897b5007610c60527e8cb135b2212d8f617
ktr0731 <ktr@syfm.me>
2019-12-29 19:17:58 +0900

    Brew formula update for evans version 0.8.5

--- evans.rb
+++ evans.rb
@@ -2,9 +2,9 @@
 class Evans < Formula
   desc "Evans: more expressive universal gRPC client"
   homepage "https://evans.syfm.me"
-  url "http://github.com/ktr0731/evans/releases/download/0.8.4/evans_darwin_amd64.tar.gz"
-  version "0.8.4"
-  sha256 "a16937b19123924b49e180a21d3e413b550ed2b0dfb7ad05bd0fc01dcc2fa8e8"
+  url "http://github.com/ktr0731/evans/releases/download/0.8.5/evans_darwin_amd64.tar.gz"
+  version "0.8.5"
+  sha256 "68f28db57274944731388e357bfe4aa82025b5d5fdbd8dc3a149613055f7f94d"
 
   def install
     bin.install "evans"
