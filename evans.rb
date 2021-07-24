c4b0571d08c0b101955bc949f22fe54549fe499e
ktr0731 <ktr@syfm.me>
2021-07-25 00:33:43 +0900

    Brew formula update for evans version 0.10.0

--- evans.rb
+++ evans.rb
@@ -5,20 +5,20 @@
 class Evans < Formula
   desc "Evans: more expressive universal gRPC client"
   homepage "https://evans.syfm.me"
-  version "0.9.3"
+  version "0.10.0"
   bottle :unneeded
 
   if OS.mac?
-    url "http://github.com/ktr0731/evans/releases/download/0.9.3/evans_darwin_amd64.tar.gz"
-    sha256 "21d17adddf2f7cec1384ae3577a6272a18237835335f9ca6ab489cc659104dec"
+    url "http://github.com/ktr0731/evans/releases/download/0.10.0/evans_darwin_amd64.tar.gz"
+    sha256 "3b5dd7ab606b922b53c77881b79cd080ccb7fe75590a5b967b30bfbf99e5c1b3"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "http://github.com/ktr0731/evans/releases/download/0.9.3/evans_linux_amd64.tar.gz"
-    sha256 "166404aab42cceee41b15a47f4f94b4d8725d689293743f767eb1bfc98d971d0"
+    url "http://github.com/ktr0731/evans/releases/download/0.10.0/evans_linux_amd64.tar.gz"
+    sha256 "d9c53f2e049fee97e8339615f5d3323021a5b68a4cdc5b1e8c271e0f203d2d74"
   end
   if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "http://github.com/ktr0731/evans/releases/download/0.9.3/evans_linux_arm.tar.gz"
-    sha256 "16d16dd27d05d16597757e89aa34043c0839e78af657ac38f8599b663e43ba46"
+    url "http://github.com/ktr0731/evans/releases/download/0.10.0/evans_linux_arm.tar.gz"
+    sha256 "a45756bf295d3c4a72779385dbf6be848f9df6d211e5558a0e9d6e43092e9091"
   end
 
   def install
